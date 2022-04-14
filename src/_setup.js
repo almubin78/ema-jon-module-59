c530800953f6b8641e90e6f599722257304a7456
Al Mubin <almubin78@gmail.com>
2022-04-14 23:31:13 +0600

    this is from 53 to 59 convert

---
+++ src/_setup.js
@@ -0,0 +1,17 @@
+/* 
+1.create a new project in console.goole.com 
+2. npm install firebase
+3. create firebase.init,js and import getAuth to export auth;
+4. firebase setting > go to authentication> email/password and enable it and save
+5.
+
+*/
+/* 
+Hosting steps in [https://console.firebase.google.com/u/0/project/ema-john-simple-ten-fiftynine/overview]
+
+1. npm install -g firebase-tools
+2. firebase login
+3. firebase init
+4. firebase deploy
+and continue to console
+*/
---
+++ src/firebase.init.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/app'; 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAthbUByCVX1MnBveFC8mKShDSBvoA6gpg",
+  authDomain: "ema-john-simple-ten-fiftynine.firebaseapp.com",
+  projectId: "ema-john-simple-ten-fiftynine",
+  storageBucket: "ema-john-simple-ten-fiftynine.appspot.com",
+  messagingSenderId: "844516567899",
+  appId: "1:844516567899:web:9fb43ace5dd7907d98487e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+export default auth;
