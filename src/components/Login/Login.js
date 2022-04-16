adb0fb792257017bcc74f6e26694a45aa4922292
Al Mubin <almubin78@gmail.com>
2022-04-16 11:11:37 +0600

    shipping added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 import './Login.css'
 const Login = () => {
@@ -13,6 +13,10 @@ const Login = () => {
         error,
     ] = useSignInWithEmailAndPassword(auth);
     const navigate =  useNavigate();
+
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
+
     const handleEmailBlur = event =>{
         setEmail(event.target.value);
     }
@@ -20,7 +24,7 @@ const Login = () => {
         setPassword(event.target.value);
     }
     if(user){
-        navigate('/shop');
+        navigate(from, {replace:true});
     }
     const handleUserSignIn = event =>{
         event.preventDefault()
